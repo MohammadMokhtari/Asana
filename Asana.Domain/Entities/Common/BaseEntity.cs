21edbdda3271c63a060273e494c5aed048ce82f0
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-02-05 10:14:33 +0330

    Created BaseEntity Model

---
+++ Asana.Domain/Entities/Common/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Asana.Domain.Entities.Common
+{
+    public class BaseEntity
+    {
+        #region Properties
+
+        public long Id { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime ModifiedOn { get; set; }
+
+        #endregion
+    }
+}
