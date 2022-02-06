ba298d2d485a70ff7dea46cdd696ca528136aa90
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-02-06 16:30:00 +0330

    Created AsanaDbContext

---
+++ Asana.Infrastructure/Persistence/Context/AsanaDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Asana.Domain.Entities.Addresses;
+using Asana.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Reflection;
+
+namespace Asana.Infrastructure.Persistence.Context
+{
+    public class AsanaDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+    {
+        public AsanaDbContext(DbContextOptions<AsanaDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+
+        public DbSet<State> States { get; set; }
+
+        public DbSet<City> Cities { get; set; }
+        
+    }
+}
