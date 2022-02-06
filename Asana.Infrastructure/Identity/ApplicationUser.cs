00d94bc4b4bafa794942decab93195f09df56dee
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-02-06 16:21:50 +0330

    Created ApplicationUser Model

---
+++ Asana.Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,33 @@
+﻿using Asana.Domain.Entities.Addresses;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Asana.Infrastructure.Identity
+{
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+
+        [StringLength(220)]
+        public string FirstName { get; set; }
+
+        [StringLength(220)]
+        public string LastName { get; set; }
+
+        [StringLength(10)]
+        public string NationalCode { get; set; }
+
+        [StringLength(16)]
+        public string CreditCardNumber { get; set; }
+
+        public string Avatar { get; set; }
+
+
+        #region Relations
+
+        public ICollection<Address> Addresses{ get; set; }
+
+        #endregion
+    }
+}
