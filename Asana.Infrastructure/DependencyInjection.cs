829e65d96b9a09011be5ddcdbc08779f51491042
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-02-06 16:33:31 +0330

    Created Infrastructure DependencyInjection
    create Infrastructure Ioc and Config AsanaDbContext

---
+++ Asana.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Asana.Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Asana.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services , IConfiguration configuration)
+        {
+            services.AddDbContext<AsanaDbContext>(option =>
+            {
+
+                option.UseSqlServer(configuration.GetConnectionString("AsanaConnectionString"));
+
+            });
+
+            return services;
+        }
+    }
+}
