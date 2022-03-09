1eea39434d61728a3d404338f8ccd359ea8dc736
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-03-09 17:40:10 +0330

    Created EmailMessage.cs

---
+++ Asana.Application/Common/Models/EmailMessage.cs
@@ -0,0 +1,23 @@
+﻿using MimeKit;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Asana.Application.Common.Models
+{
+    public class EmailMessage
+    {
+        public EmailMessage(IEnumerable<string> to, string subject, string content)
+        {
+            To = new List<MailboxAddress>();
+
+            To.AddRange(to.Select(x => new MailboxAddress(x)));
+            Subject = subject;
+            Content = content;
+        }
+
+        public List<MailboxAddress> To { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+
+    }
+}
