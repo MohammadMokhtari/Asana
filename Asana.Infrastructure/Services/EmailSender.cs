2cb7cc184acea9f14a05a7cc699127339bd47456
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-03-09 17:43:30 +0330

    Added EmailSenderService and Impliment IEmailSender Interface
    impliment IEmailSenderService interface and complate email send logic

---
+++ Asana.Infrastructure/Services/EmailSender.cs
@@ -0,0 +1,145 @@
+﻿using Asana.Application.Common.Enums;
+using Asana.Application.Common.Interfaces;
+using Asana.Application.Common.Models;
+using Asana.Infrastructure.Persistence.Options;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Asana.Infrastructure.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly ILogger<EmailSender> _logger;
+        private readonly EmailOptions _emailConfig;
+        private readonly IWebHostEnvironment _env;
+
+        public EmailSender(ILogger<EmailSender> logger, IOptions<EmailOptions> options, IWebHostEnvironment env)
+        {
+            _logger = logger;
+            _emailConfig = options.Value;
+            _env = env;
+        }
+
+        public void SendEmail(EmailMessage emailMessage , EmailType type)
+        {
+            var message = CreateEmailMessage(emailMessage,type);
+            send(message);
+        }
+
+        public async Task SendEmailAsync(EmailMessage emailMessage , EmailType type)
+        {
+            var message = CreateEmailMessage(emailMessage,type);
+            await SendAsync(message);
+        }
+
+
+
+        private MimeMessage CreateEmailMessage(EmailMessage message ,EmailType type)
+        {
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress(_emailConfig.Name,_emailConfig.From));
+            emailMessage.To.AddRange(message.To);
+            emailMessage.Subject = message.Subject;
+
+            var messageBody = "";
+            switch(type)
+            {
+                case EmailType.verifiedEmail:
+                     messageBody = CreateEmailContent(message.Content, "verifiedEmail.html");
+
+                  emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html) 
+                  { Text = messageBody }; 
+                    break;
+
+                case EmailType.resetPassword:
+
+                     messageBody = CreateEmailContent(message.Content, "resetPassword.html");
+
+                    emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+                    { Text = messageBody };
+                    break;
+            }
+            
+            return emailMessage;
+        }
+
+
+        private string CreateEmailContent(string Link , string templateName)
+        {
+            var templatePath = _env.WebRootPath
+                + Path.DirectorySeparatorChar.ToString()
+                + "templates"
+                + Path.DirectorySeparatorChar.ToString()
+                + "EmailTemplate"
+                + Path.DirectorySeparatorChar.ToString()
+                + templateName;
+
+            var builder = new BodyBuilder();
+
+            using (StreamReader stream = new StreamReader(templatePath))
+            {
+                builder.HtmlBody = stream.ReadToEnd();
+            }
+            string messageBody = string.Format(builder.HtmlBody,Link);
+
+            return messageBody;
+        }
+
+
+        private async Task SendAsync(MimeMessage mailMessage)
+        {
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    await client.ConnectAsync(_emailConfig.SmtpServer, _emailConfig.Port, true);
+                    client.AuthenticationMechanisms.Remove("XOAUTH2");
+                    await client.AuthenticateAsync(_emailConfig.UserName, _emailConfig.Password);
+
+                    await client.SendAsync(mailMessage);
+                }
+                catch (Exception)
+                {
+                    _logger.LogError("Cant Send Email");
+                    throw;
+                }
+                finally
+                {
+                    await client.DisconnectAsync(true);
+                    client.Dispose();
+                }
+            }
+        }
+
+        public void send(MimeMessage mailMessage)
+        {
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    client.Connect(_emailConfig.SmtpServer, _emailConfig.Port, true);
+                    client.AuthenticationMechanisms.Remove("XOAUTH2");
+                    client.Authenticate(_emailConfig.UserName, _emailConfig.Password);
+
+                    client.Send(mailMessage);
+                }
+                catch (Exception)
+                {
+                    _logger.LogError("Cant Send Email");
+                    throw;
+                }
+                finally
+                {
+                    client.Disconnect(true);
+                    client.Dispose();
+                }
+            }
+        }
+    }
+}
