f04edeb9fa9745d5341409318f65ac3f6aefd138
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-03-09 18:26:00 +0330

    Added Identity COnfiguration in AsanaSbContext.cs

--- Asana.Infrastructure/Persistence/Context/AsanaDbContext.cs
+++ Asana.Infrastructure/Persistence/Context/AsanaDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Asana.Domain.Entities.Addresses;
 using Asana.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -18,6 +19,46 @@ namespace Asana.Infrastructure.Persistence.Context
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            #region Identity Configuration
+
+            builder.Entity<IdentityUser>(b =>
+            {
+                b.ToTable("Users");
+            });
+
+            builder.Entity<IdentityUserClaim<string>>(b =>
+            {
+                b.ToTable("UserClaims");
+            });
+
+            builder.Entity<IdentityUserLogin<string>>(b =>
+            {
+                b.ToTable("UserLogins");
+            });
+
+            builder.Entity<IdentityUserToken<string>>(b =>
+            {
+                b.ToTable("UserTokens");
+            });
+
+            builder.Entity<IdentityRole>(b =>
+            {
+                b.ToTable("Roles");
+            });
+
+            builder.Entity<IdentityRoleClaim<string>>(b =>
+            {
+                b.ToTable("RoleClaims");
+            });
+
+            builder.Entity<IdentityUserRole<string>>(b =>
+            {
+                b.ToTable("UserRoles");
+            });
+
+            #endregion
+
 
             builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         }
