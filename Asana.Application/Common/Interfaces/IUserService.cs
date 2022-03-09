20f9dc2a242dc5ae1ca98b5f074fef0bb4a5cd27
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-03-09 20:48:31 +0330

    Create IUserService Interface

---
+++ Asana.Application/Common/Interfaces/IUserService.cs
@@ -0,0 +1,21 @@
+﻿using Asana.Application.Common.Models;
+using Asana.Application.DTOs;
+using System.Threading.Tasks;
+
+namespace Asana.Application.Common.Interfaces
+{
+    public interface IUserService
+    {
+        Task<Result> RegisterAsync(UserRegisterDTO userRegister);
+
+        Task<Result> ConfirmEmailAsync(string token, string userId);
+
+        Task<Result> LoginAsync(UserLoginDTO userLogin);
+
+        Task<Result> GetUserByIdAsync(string id);
+
+        Task<Result> ForgotPasswordAsync(string email);
+
+        Task<Result> ResetPasswordAsync(ResetPasswordDTO passwordDTO);
+    }
+}
