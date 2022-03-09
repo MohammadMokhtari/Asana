a0abcb959eb45cccc6b8694c9d5c125c03fd0d3e
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-03-09 20:40:26 +0330

    Added ForgotPasswordDTO and ResetPasswordDTO

--- Asana.Application/DTOs/UsersDTOs.cs
+++ Asana.Application/DTOs/UsersDTOs.cs
@@ -18,5 +18,19 @@
         public bool IsRemember { get; set; }
     }
 
-   
+    public class ForgotPasswordDTO
+    {
+        public string Email { get; set; }
+    }
+
+    public class ResetPasswordDTO
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+
+        public string Code { get; set; }
+    }
 }
