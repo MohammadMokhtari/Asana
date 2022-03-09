9a63929b2ed843da66da0db0b2e0285845248b12
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-03-09 17:40:45 +0330

    Created EmailType enum

---
+++ Asana.Application/Common/Enums/Email.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asana.Application.Common.Enums
+{
+    public enum EmailType
+    {
+       verifiedEmail ,
+       resetPassword ,
+
+    }
+}
