b7d61ca8b2af445a8c262fa3151dccccbfc94711
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-03-09 18:14:21 +0330

    Config JWT Configurration

--- Asana.Infrastructure/DependencyInjection.cs
+++ Asana.Infrastructure/DependencyInjection.cs
@@ -3,10 +3,13 @@ using Asana.Infrastructure.Identity;
 using Asana.Infrastructure.Persistence.Context;
 using Asana.Infrastructure.Persistence.Options;
 using Asana.Infrastructure.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Asana.Infrastructure
 {
@@ -42,7 +45,33 @@ namespace Asana.Infrastructure
             #endregion
 
 
-           
+            #region Authentication
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+              .AddJwtBearer(options =>
+                {
+                    options.SaveToken = true;
+                    options.RequireHttpsMetadata = false;
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidateAudience = true,
+                        LifetimeValidator = TokenLifetimeValidator.Validate,
+                        ValidAudience = configuration["Jwt:Audience"],
+                        ValidIssuer = configuration["Jwt:Issuer"],
+                        IssuerSigningKey = 
+                        new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"])),
+                    };
+                });
+
+            #endregion
             return services;
         }
     }
