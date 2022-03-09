c146b840c539eb28b8a1b00266e599d48ca35d92
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-03-09 17:59:19 +0330

    Added Result.cs

---
+++ Asana.Application/Common/Models/Result.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Asana.Application.Common.Models
+{
+    public class Result
+    {
+        internal Result(bool succeeded, IEnumerable<string> errors , object value = default)
+        {
+            Succeeded = succeeded;
+            Errors = errors.ToArray();
+            Value = value;
+        }
+
+        public bool Succeeded { get; set; }
+
+        public string[] Errors { get; set; }
+
+        public object Value { get; set; }
+
+        public static Result Success(object value = default)
+        {
+            return new Result(true, Array.Empty<string>() , value);
+        }
+
+        public static Result Failure(IEnumerable<string> errors)
+        {
+            return new Result(false, errors);
+        }
+    }
+}
