2322ed395e3cbcc180c288f448fa19a3474b0714
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-03-09 17:58:25 +0330

    Created UserDTOs (RegisterDTO - LoginDTO)

---
+++ Asana.Application/DTOs/UsersDTOs.cs
@@ -0,0 +1,22 @@
+﻿namespace Asana.Application.DTOs
+{
+    public class UserRegisterDTO
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmPasword { get; set; }
+    }
+
+    public class UserLoginDTO
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public bool IsRemember { get; set; }
+    }
+
+   
+}
