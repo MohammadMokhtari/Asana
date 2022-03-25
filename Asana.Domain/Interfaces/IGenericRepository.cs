649a493169f1024b3c3140403d7f824c2789ae91
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-03-25 20:52:41 +0430

    Created IGenericRepository interfaces

---
+++ Asana.Domain/Interfaces/IGenericRepository.cs
@@ -0,0 +1,24 @@
+﻿using Asana.Domain.Entities.Common;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asana.Domain.Interfaces
+{
+    public interface IGenericRepository<TEntity> : IDisposable where TEntity : BaseEntity , ISoftDeletable
+    {
+        IQueryable<TEntity> GetEntitiesQuery();
+
+        Task<TEntity> GetEntityByIdAsync(int id);
+
+        Task AddEntityAsync(TEntity entity);
+
+        void UpdateEntity(TEntity entity);
+
+        void RemoveEntity(TEntity entity);
+
+        Task RemoveEntity(int id);
+
+        Task SaveChangeAsync();
+    }
+}
