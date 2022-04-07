826c1cd9caf48369de95c117395c5dbf9bbca930
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-04-07 12:06:46 +0430

    Created ILocationService interface

---
+++ Asana.Application/Common/Interfaces/ILocationService.cs
@@ -0,0 +1,13 @@
+﻿using Asana.Application.Common.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace Asana.Application.Common.Interfaces
+{
+    public interface ILocationService
+    {
+        Task<Result> GetLocationsAsync(Guid userId);
+
+        Task<Result> SetDefaultLocationAsync(long locationId, Guid userId);
+    }
+}
