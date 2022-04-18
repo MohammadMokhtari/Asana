9d4c626dc36191762fe9349629723299b865e8b2
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-04-18 10:45:13 +0430

    Created UserLoginResponseDto & LoginResponseDto & UserPersonalInfoDto &UserPersonalInfoDto and UserPhotoUpdatDto

--- Asana.Application/DTOs/UsersDTOs.cs
+++ Asana.Application/DTOs/UsersDTOs.cs
@@ -1,4 +1,8 @@
-﻿namespace Asana.Application.DTOs
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+
+namespace Asana.Application.DTOs
 {
     public class UserRegisterDto
     {
@@ -33,4 +37,93 @@
 
         public string Code { get; set; }
     }
+
+    public class UserLoginResponseDto
+    {
+        public string UserId { get; set; }
+
+        public string Email { get; set; }
+
+        public string UserName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FullName { get => FirstName + ' ' + LastName; }
+
+        public string PhotoUrl { get; set; }
+
+        public double WalletBalance { get; set; }
+
+        public string Mobile { get; set; }
+
+        public int Score { get; set; }
+        
+        public string Token { get; set; }
+
+        public int TokenExpiresIn { get; set; }
+    }
+
+    public class LoginResponseDto
+    {
+        public UserLoginResponseDto User { get; set; }
+
+        public AddressDto DefaultAddress { get; set; }
+    }
+
+    public class UserPersonalInfoDto
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FullName { get => FirstName + ' ' + LastName; }
+
+        public string PhotoUrl { get; set; }
+
+        public string Email { get; set; }
+
+        public string UserName { get; set; }
+
+        public string NationalCode { get; set; }
+
+        public string Gender { get; set; }
+
+        public string CreditCardNumber { get; set; }
+
+        public string Mobile { get; set; }
+
+        public int Score { get; set; }
+        
+        public double WalletBalance { get; set; }
+
+        public string CreatedOn { get; set; }
+
+        public string ModifiedOn { get; set; }
+
+        public List<AddressDto> Addresses { get; set; }
+    }
+
+    public class UserUpdatDto
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string NationalCode { get; set; }
+
+        public string CreditCardNumber { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Gender { get; set; }
+
+    }
+    public class UserPhotoUpdatDto
+    {
+        public string PhotoUrl { get; set; }
+
+    }
+
 }
