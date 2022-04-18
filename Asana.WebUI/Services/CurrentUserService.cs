95dbb9c3b8fb3ee1cf369b49540affd692a4392d
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-04-18 10:08:39 +0430

    Created CurrentUserService and implement ICurrentUserService

---
+++ Asana.WebUI/Services/CurrentUserService.cs
@@ -0,0 +1,19 @@
+﻿using Asana.Application.Common.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Security.Claims;
+
+namespace Asana.WebUI.Services
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor) =>
+            _httpContextAccessor = httpContextAccessor;
+
+        public string UserId => _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        public Guid GuidUserId => Guid.Parse(_httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier));
+    }
+}
