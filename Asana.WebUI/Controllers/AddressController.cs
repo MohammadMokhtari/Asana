81bf8e994b66e7b0d113bd5a75d30811cc028962
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-04-18 10:13:59 +0430

    Renamed LocationController to AddressController

---
+++ Asana.WebUI/Controllers/AddressController.cs
@@ -0,0 +1,78 @@
+﻿using Asana.Application.Common.Interfaces;
+using Asana.Application.DTOs;
+using Asana.Application.Utilities.Common;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Asana.WebUI.Controllers
+{
+    [Authorize]
+    public class AddressController : ApiControllerBase
+    {
+        private readonly IAddressService _addressService;
+        private readonly IProvinceService _provinceService;
+
+        public AddressController(IAddressService addressService, IProvinceService provinceService)
+        {
+            _addressService = addressService;
+            _provinceService = provinceService;
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> index()
+        {
+            var result = await _addressService.GetAddressesAsync();
+
+            return result.Succeeded ? JsonResponseStatus.Success(result.Value)
+                : JsonResponseStatus.BadRequest(result.Errors);
+        }
+
+        [HttpGet("setAddress/{locationId}")]
+        public async Task<IActionResult> SetDefaultAddress(long locationId)
+        {
+
+            var result = await _addressService.
+                SetDefaultAddressAsync(locationId);
+
+            return result.Succeeded ? JsonResponseStatus.Success(result.Value)
+                : JsonResponseStatus.BadRequest(result.Errors);
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateAddress(AddressCreateDto addressDto)
+        {
+            var result = await _addressService.CreateAddressAsync(addressDto);
+
+            return result.Succeeded ? JsonResponseStatus.SuccessCreated() :
+                JsonResponseStatus.Error(result.Errors);
+        }
+
+        [HttpPut("update")]
+        public async Task<IActionResult> UpdateAddress(AddressUpdateDto addressDto)
+        {
+            var result = await _addressService.UpdateAddressAsync(addressDto);
+
+            return result.Succeeded ? JsonResponseStatus.Success() :
+                JsonResponseStatus.NotFound(result.Errors);
+        }
+
+        [HttpDelete("delete/{id}")]
+        public async Task<IActionResult> DeleteAddress(long id)
+        {
+            var result = await _addressService.DeleteAddressAsync(id);
+            return result.Succeeded ? JsonResponseStatus.Success() :
+                JsonResponseStatus.Error();
+        }
+
+        [HttpGet("allProvince")]
+        public async Task<IActionResult> AllProvince()
+        {
+            var result = await _provinceService.AllProvinceOptionAsync();
+
+            return result.Succeeded ? JsonResponseStatus.Success(result.Value) :
+                            JsonResponseStatus.Error();
+        }
+
+    }
+}
