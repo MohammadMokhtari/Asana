164d69d03a21bc2e4e2cc1d229eaa3fa00a70cf0
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-04-18 10:39:03 +0430

    Added CreateData&updateData in ApplicationUser Migration and update Database

---
+++ Asana.Infrastructure/Persistence/Migrations/20220413101204_createData-updateData_ColumnInUserApplication.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Asana.Infrastructure.Persistence.Migrations
+{
+    public partial class createDataupdateData_ColumnInUserApplication : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifiedOn",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2022, 4, 13, 14, 42, 4, 390, DateTimeKind.Local).AddTicks(9130),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedOn",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2022, 4, 13, 14, 42, 4, 386, DateTimeKind.Local).AddTicks(379),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifiedOn",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2022, 4, 13, 14, 42, 4, 390, DateTimeKind.Local).AddTicks(9130));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedOn",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2022, 4, 13, 14, 42, 4, 386, DateTimeKind.Local).AddTicks(379));
+        }
+    }
+}
