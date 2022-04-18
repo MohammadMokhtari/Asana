269331d89bf6a00c9d192d6ddbd47355a6b41668
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-04-18 10:10:01 +0430

    Added UrlBuilderService and implenent IUrlBuilderService

---
+++ Asana.Application/Common/Services/UrlBuilderService.cs
@@ -0,0 +1,26 @@
+﻿using Asana.Application.Common.Interfaces;
+using Asana.Domain.Entities.Media;
+
+namespace Asana.Application.Common.Services
+{
+    public class UrlBuilderService : IUrlBuilderService
+    {
+        private readonly IHostService _hostService;
+
+        public UrlBuilderService(IHostService hostService) => _hostService = hostService;
+
+        public string BlankProfilePhotoUrl()
+        {
+            return $"https://{_hostService.HostName}/media/images/profile/blank.jpg";
+        }
+
+
+        public string BuildAbsolutProfilePhotoUrl(UserMediaFile file)
+        {
+            if (file is null)
+                return BlankProfilePhotoUrl();
+
+            return $"https://{_hostService.HostName}{file.FolderPath}/Thumbnail{file.MediaName}";
+        }
+    }
+}
