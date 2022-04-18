a2480f0e7c932b70a057504ce3a57e5bb3a7dbee
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-04-18 10:42:08 +0430

    Added UserUpdateDtoValidation

--- Asana.Application/Common/Validations/UsersValidations.cs
+++ Asana.Application/Common/Validations/UsersValidations.cs
@@ -80,4 +80,41 @@ namespace Asana.Application.Common.Validations
                 .NotEmpty();
         }
     }
+
+    public class UserUpdateDtoValidation : AbstractValidator<UserUpdatDto>
+    {
+        public UserUpdateDtoValidation()
+        {
+            RuleFor(a => a.FirstName)
+                .MaximumLength(220)
+                .When(a=>!string.IsNullOrEmpty(a.FirstName))
+                .WithMessage("حداکثر طول مجاز 220 کارکتر می باشد")
+                .MinimumLength(3)
+                .When(a=>!string.IsNullOrEmpty(a.FirstName))
+                .WithMessage("حداقل طول مجاز 220 کارکتر می باشد");
+
+            RuleFor(a => a.LastName)
+                .MaximumLength(220)
+                .When(a=>!string.IsNullOrEmpty(a.LastName))
+                .WithMessage("حداکثر طول مجاز 220 کارکتر می باشد")
+                 .MinimumLength(3)
+                .When(a => !string.IsNullOrEmpty(a.LastName))
+                .WithMessage("حداقل طول مجاز 220 کارکتر می باشد");
+
+            RuleFor(a => a.NationalCode).Matches("^[0-9]{10}$")
+                .When(a => !string.IsNullOrEmpty(a.NationalCode))
+                .WithMessage("کد ملی وارد شده معتبر نمی باشد");
+
+            RuleFor(a => a.PhoneNumber).Matches(@"^(\+98|0)?9\d{9}$")
+                .When(a => !string.IsNullOrEmpty(a.PhoneNumber))
+              .WithMessage("شماره تلفن وارد شده معتبر نمی باشد");
+
+            RuleFor(a => a.CreditCardNumber).Matches("^[0-9]{16}$")
+              .When(a => !string.IsNullOrEmpty(a.CreditCardNumber))
+              .WithMessage("شماره کارت بانکی وارد شده معتبر نمی باشد");
+
+            RuleFor(a => a.Gender).NotNull().WithMessage("جنسیت وارد شده معتبر نمی باشد")
+                .Must(a=>a.Equals("Male") || a.Equals("Female") || a.Equals("Unknown"));
+        }
+    }
 }
