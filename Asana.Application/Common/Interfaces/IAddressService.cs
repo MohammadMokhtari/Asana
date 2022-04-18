82d84c9d649dd46a62165821b2a5c6d035f63a19
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-04-18 09:33:52 +0430

    Renamed ILocationService to IAddressService
    renamed ILocationService to IAddressService and Rename Methods and Added a new Methods

---
+++ Asana.Application/Common/Interfaces/IAddressService.cs
@@ -0,0 +1,20 @@
+﻿using Asana.Application.Common.Models;
+using Asana.Application.DTOs;
+using System.Threading.Tasks;
+
+namespace Asana.Application.Common.Interfaces
+{
+    public interface IAddressService
+    {
+        Task<Result> GetAddressesAsync();
+
+        Task<Result> SetDefaultAddressAsync(long addressId);
+
+        Task<Result> CreateAddressAsync(AddressCreateDto addressDto);
+
+        Task<Result> UpdateAddressAsync(AddressUpdateDto addressDto);
+
+        Task<Result> DeleteAddressAsync(long addressId);
+
+    }
+}
