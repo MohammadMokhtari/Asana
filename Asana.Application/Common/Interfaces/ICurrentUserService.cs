040597279b802952059e54e08ab0d39c3f6b1074
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-04-18 10:07:35 +0430

    Added ICurrentUserService Inteface

---
+++ Asana.Application/Common/Interfaces/ICurrentUserService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asana.Application.Common.Interfaces
+{
+    public interface ICurrentUserService
+    {
+        string UserId { get; }
+
+        Guid GuidUserId { get; }
+    }
+}
