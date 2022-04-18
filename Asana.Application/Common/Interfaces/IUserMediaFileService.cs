e279d6f81959747e4caafce113cac868fd55e294
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-04-18 10:10:29 +0430

    Added IUserMediaFileService Interface

---
+++ Asana.Application/Common/Interfaces/IUserMediaFileService.cs
@@ -0,0 +1,14 @@
+﻿using Asana.Application.Common.Models;
+using System.Threading.Tasks;
+
+namespace Asana.Application.Common.Interfaces
+{
+    public interface IUserMediaFileService
+    {
+        Task<Result> UpdatUserPhotoAsync(ProcessImageModel image);
+
+        Task<Result> DeleteUserPhotoAsync();
+
+        Task<Result> GetPhotoUrl();
+    }
+}
