bfde39e39435c62b4bdcbe6a19f22b0b77b3f71e
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-04-18 10:17:38 +0430

    Created IProvinceService Interface

---
+++ Asana.Application/Common/Interfaces/IProvinceService.cs
@@ -0,0 +1,11 @@
+﻿using Asana.Application.Common.Models;
+using System.Threading.Tasks;
+
+namespace Asana.Application.Common.Interfaces
+{
+    public interface IProvinceService
+    {
+        Task<Result> AllProvinceOptionAsync();
+
+    }
+}
