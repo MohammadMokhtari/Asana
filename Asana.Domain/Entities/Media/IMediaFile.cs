497db0b06f9895a5843dccd2361b704af13e4851
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-04-18 10:31:33 +0430

    Added MediaFiles Domain Entities

---
+++ Asana.Domain/Entities/Media/IMediaFile.cs
@@ -0,0 +1,23 @@
+﻿using Asana.Domain.Entities.Common;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Asana.Domain.Entities.Media
+{
+    public interface IMediaFile 
+    {
+
+        public string MediaName { get; set; }
+
+        public string Alt { get; set; }
+
+        public string FolderPath { get; set; }
+
+        public bool IsDelete { get; set; }
+
+        public int MediaTypeId { get; set; }
+
+        public MediaType Type { get; set; }
+
+    }
+}
---
+++ Asana.Domain/Entities/Media/UserMediaFile.cs
@@ -0,0 +1,33 @@
+﻿using Asana.Domain.Entities.Common;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Asana.Domain.Entities.Media
+{
+    public class UserMediaFile : BaseEntity, IMediaFile, ISoftDeletable
+    {
+        #region Properties
+
+        public string MediaName { get; set; }
+
+        public string Alt { get; set; }
+
+        public string FolderPath { get; set; }
+
+        public bool IsDelete { get; set; }
+
+        public int MediaTypeId { get; set; }
+
+        public Guid UserId { get; set; }
+
+        [NotMapped]
+        public MediaType Type
+        {
+            get => (MediaType)MediaTypeId;
+            set => MediaTypeId = (int)value;
+        }
+
+        #endregion
+
+    }
+}
