fee22cfbbae2cadd34c5ebd3f7f87a435983dcb2
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-04-18 09:43:12 +0430

    Created IApplicationDbContext Interface

---
+++ Asana.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Asana.Domain.Entities.Addresses;
+using Asana.Domain.Entities.Media;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Asana.Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        public DbSet<Address> Addresses { get; set; }
+
+        public DbSet<State> States { get; set; }
+
+        public DbSet<City> Cities { get; set; }
+
+        public DbSet<UserMediaFile> UserMediaFiles { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+
+    }
+}
