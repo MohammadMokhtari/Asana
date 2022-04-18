d60067051d66e08b735e65b9df075f429d7200d2
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-04-18 09:39:24 +0430

    Added ProcessImageModel

---
+++ Asana.Application/Common/Models/ProcessImageModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asana.Application.Common.Models
+{
+    public class ProcessImageModel
+    {
+        public string FileName { get; set; }
+
+        public string Type { get; set; }
+
+        public Stream Content { get; set; }
+    }
+}
