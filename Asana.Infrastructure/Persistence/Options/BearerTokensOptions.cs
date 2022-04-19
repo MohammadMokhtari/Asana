79260565e7e21101ef3272b79a66cec7a47e8135
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-04-19 18:32:50 +0430

    Created BearerTokenOptions Configuration

---
+++ Asana.Infrastructure/Persistence/Options/BearerTokensOptions.cs
@@ -0,0 +1,17 @@
+﻿namespace Asana.Infrastructure.Persistence.Options
+{
+   public class BearerTokensOptions
+    {
+        public const string JWtBearer = "JWtBearer";
+
+        public string Key { set; get; }
+
+        public string Issuer { set; get; }
+
+        public string Audience { set; get; }
+
+        public int AccessTokenExpirationSeconds { set; get; }
+
+        public int RefreshTokenExpirationSeconds { set; get; }
+    }
+}
