df5c969665fa2d6082a948dc4428d99d5a3a8792
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-04-19 18:30:53 +0430

    Created RefreshToken Entity

---
+++ Asana.Domain/Entities/Token/RefreshToken.cs
@@ -0,0 +1,23 @@
+﻿using Asana.Domain.Entities.Common;
+using System;
+
+namespace Asana.Domain.Entities.Token
+{
+    public class RefreshToken : BaseEntity, ISoftDeletable
+    {
+        public string Token { get; set; }
+
+        public string JwtId { get; set; }
+
+        public DateTimeOffset RefreshTokenExpiresDate { get; set; }
+
+        public bool IsDelete { get; set; }
+
+        public bool Used { get; set; }
+
+        public bool Invalidated { get; set; }
+
+        public Guid UserId { get; set; }
+
+    }
+}
