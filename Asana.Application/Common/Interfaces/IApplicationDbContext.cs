ca0eaa23349d0136442bd7df2f1e76e0cbd79f80
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-04-25 00:59:43 +0430

    Added new  DbSet in IApplicationDbContex
    Added Products &MediaFile & category dbset in IApplicationDbContext interface

--- Asana.Application/Common/Interfaces/IApplicationDbContext.cs
+++ Asana.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Asana.Domain.Entities.Addresses;
+using Asana.Domain.Entities.Categories;
 using Asana.Domain.Entities.Media;
+using Asana.Domain.Entities.Products;
 using Asana.Domain.Entities.Token;
 using Microsoft.EntityFrameworkCore;
 using System.Threading;
@@ -18,6 +20,26 @@ namespace Asana.Application.Common.Interfaces
         public DbSet<UserMediaFile> UserMediaFiles { get; set; }
 
         public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+        public DbSet<MediaFile> MediaFiles { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Color> Colors { get; set; }
+        
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<ProductMediaFile> ProductMediaFiles { get; set; }
+
+        public DbSet<ProductReview> ProductReviews { get; set; }
+
+        public DbSet<ProductReviewHelpfulness> ProductReviewHelpfulnesses { get; set; }
+
+        public DbSet<ProductTag> ProductTags { get; set; }
+
+        public DbSet<ProductWarranti> ProductWarrantis { get; set; }
+
+        public DbSet<SpecializedProductReview> SpecializedProductReviews { get; set; }
 
         Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 
