7b5658ccaefd7b9201954eab7070122754d0a5db
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-04-25 01:01:18 +0430

    Added Migration and Update DataBase
    Added AllProducts Table and MediaFile and Category Tables

--- Asana.Infrastructure/Persistence/Migrations/20220424201423_Added Products and Category Table.Designer.cs
+++ Asana.Infrastructure/Persistence/Migrations/20220424201423_Added Products and Category Table.Designer.cs
@@ -4,14 +4,16 @@ using Asana.Infrastructure.Persistence.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Asana.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(AsanaDbContext))]
-    partial class AsanaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220424201423_Added Products and Category Table")]
+    partial class AddedProductsandCategoryTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
