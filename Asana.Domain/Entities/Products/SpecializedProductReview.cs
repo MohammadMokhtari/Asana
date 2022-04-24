012842e282e00b260c88d78739c8b34f560a7e4e
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-04-25 00:55:44 +0430

    Added SpecializedProductReview Entity

---
+++ Asana.Domain/Entities/Products/SpecializedProductReview.cs
@@ -0,0 +1,23 @@
+﻿using Asana.Domain.Entities.Common;
+
+namespace Asana.Domain.Entities.Products
+{
+    public class SpecializedProductReview : BaseEntity, ISoftDeletable
+    {
+        #region Properties
+
+        public string SpecializedReviewText { get; set; }
+
+        public long ProductId { get; set; }
+
+        public bool IsDelete { get; set; }
+        #endregion
+
+        #region Relations
+
+        public Product Product { get; set; }
+
+        #endregion
+
+    }
+}
